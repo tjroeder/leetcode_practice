5429710695f13374b2d20ace80ec15760f5c5731
Tim Roeder <timothyjroeder@gmail.com>
2022-03-23 22:02:43 -0600

    faster meeting room

--- 252_meeting_rooms/lib/meeting_rooms.rb
+++ 252_meeting_rooms/lib/meeting_rooms.rb
@@ -3,6 +3,6 @@ require 'pry'
 # @param {Integer[][]} intervals
 # @return {Boolean}
 def can_attend_meetings(intervals)
-  first_sort = intervals.sort_by { |meet| meet[0] }
+  first_sort = intervals.sort
   intervals.flatten.sort == first_sort.flatten
 end

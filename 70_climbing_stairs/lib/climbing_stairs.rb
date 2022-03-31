2bf863e6dbd81336ea1812c80069b8c7f29a02e4
Tim Roeder <timothyjroeder@gmail.com>
2022-03-30 19:02:29 -0600

    recursive method for climbing stairs

--- 70_climbing_stairs/lib/climbing_stairs.rb
+++ 70_climbing_stairs/lib/climbing_stairs.rb
@@ -3,10 +3,25 @@ require 'pry'
 # @param {Integer} n
 # @return {Integer}
 
+# def climb_stairs(n)
+#   res = [0, 1] # 0, 1, 1, 2, 3
+#   n.times do
+#     res << (res[-1] + res[-2])
+#   end
+#   res.last
+# end
+
 def climb_stairs(n)
-  res = [0, 1]
-  n.times do
-      res << (res[-1] + res[-2])
-  end
-  res.last
+  store = {}
+  
+  return fib(n - 1, store) + fib(n - 2, store)
+end
+
+def fib(n, store)
+  return 1 if n == 1 || n == 0
+  return store[n] if store[n]
+  
+  store[n - 1] = fib(n - 1, store)
+  store[n - 2] = fib(n - 2, store)
+  return store[n - 1] + store[n - 2]
 end
